1a3905543404390d93410849709a6a7946e5ae75
Martyna Rysak <m.rysak@wp.pl>
2015-10-13 18:03:51 +0200

    Initial Commit

---
+++ wspolrzedne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wspolrzedne
+//
+//  Created by Martyna Rysak on 13.10.2015.
+//  Copyright (c) 2015 Martyna Rysak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
